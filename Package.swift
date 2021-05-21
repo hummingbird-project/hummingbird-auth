676184e9d36509349a0087f3380c4579403a2eca
Adam Fowler <adamfowler71@gmail.com>
2021-05-21 11:11:33 +0100

    Add HummingbirdAuthXCT
    Allows tests to add authentication methods to XCTExecute
    Also fixed testMultipleBcrypt to avoid using AsyncHTTPClient

--- Package.swift
+++ Package.swift
@@ -8,9 +8,10 @@ let package = Package(
     platforms: [.macOS(.v10_15), .iOS(.v12), .tvOS(.v12)],
     products: [
         .library(name: "HummingbirdAuth", targets: ["HummingbirdAuth"]),
+        .library(name: "HummingbirdAuthXCT", targets: ["HummingbirdAuthXCT"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.11.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.11.3"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
         .package(url: "https://github.com/swift-extras/swift-extras-base64.git", .upToNextMinor(from: "0.7.0")),
     ],
@@ -21,9 +22,13 @@ let package = Package(
             .product(name: "ExtrasBase64", package: "swift-extras-base64"),
             .product(name: "Hummingbird", package: "hummingbird"),
         ]),
+        .target(name: "HummingbirdAuthXCT", dependencies: [
+            .byName(name: "HummingbirdAuth"),
+        ]),
         .target(name: "CBcrypt", dependencies: []),
         .testTarget(name: "HummingbirdAuthTests", dependencies: [
             .byName(name: "HummingbirdAuth"),
+            .byName(name: "HummingbirdAuthXCT"),
             .product(name: "HummingbirdXCT", package: "hummingbird"),
         ]),
     ]
