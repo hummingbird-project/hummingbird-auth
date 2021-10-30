1dda6b737e137c483a5304f4dbe1f385dfb9a360
Adam Fowler <adamfowler71@gmail.com>
2021-10-30 11:33:20 +0100

    HBSendable

--- Sources/HummingbirdAuth/Authenticator/Authenticator.swift
+++ Sources/HummingbirdAuth/Authenticator/Authenticator.swift
@@ -16,7 +16,7 @@ import Hummingbird
 import NIOCore
 
 /// Protocol for objects that can be returned by an `HBAuthenticator`.
-public protocol HBAuthenticatable: Sendable {}
+public protocol HBAuthenticatable: HBSendable {}
 
 /// Authenticators are middleware that are used to check if a request is authenticated and then augment the request with the
 /// authentication data.
