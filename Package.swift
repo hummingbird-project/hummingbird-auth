b314a47fc58077fd5e9fc495403e91af9f7978e3
Adam Fowler <adamfowler71@gmail.com>
2023-09-22 09:11:11 +0200

    swift-crypto v3

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", branch: "2.x.x"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"3.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
         .package(url: "https://github.com/swift-extras/swift-extras-base64.git", .upToNextMinor(from: "0.7.0")),
     ],
