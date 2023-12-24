772a14fc822a9106445d6c4efc92ee53cdece10f
Ronald Mannak <ronaldmannak@me.com>
2023-12-24 15:52:15 -0800

    Update to Swift-Crypto 3.x

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "hummingbird-auth",
-    platforms: [.macOS(.v10_15), .iOS(.v12), .tvOS(.v12)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13)],
     products: [
         .library(name: "HummingbirdAuth", targets: ["HummingbirdAuth"]),
         .library(name: "HummingbirdAuthXCT", targets: ["HummingbirdAuthXCT"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "1.8.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"3.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
         .package(url: "https://github.com/swift-extras/swift-extras-base64.git", .upToNextMinor(from: "0.7.0")),
     ],
