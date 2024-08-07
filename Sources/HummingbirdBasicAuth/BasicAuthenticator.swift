e8e1233f20f06e5d830720e0e45d7b4b82ef5305
Adam Fowler <adamfowler71@gmail.com>
2024-08-07 10:57:43 +0100

    BasicAuthenticator

---
+++ Sources/HummingbirdBasicAuth/BasicAuthenticator.swift
@@ -0,0 +1,51 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2024 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Bcrypt
+import Hummingbird
+import HummingbirdAuth
+import NIOPosix
+
+/// Protocol for user extracted from Storage
+protocol BasicUser: Authenticatable {
+    var username: String { get }
+    var passwordHash: String? { get }
+}
+
+/// Protocol for User storage
+protocol UserRepository: Sendable {
+    associatedtype User: BasicUser
+
+    func getUser(named name: String) async throws -> User?
+}
+
+/// Basic password authenticator
+///
+/// Extract username and password from "Authorization" header and checks user exists and that the password is correct
+struct BasicAuthenticator<Context: AuthRequestContext, Repository: UserRepository>: AuthenticatorMiddleware {
+    let users: Repository
+
+    func authenticate(request: Request, context: Context) async throws -> Repository.User? {
+        // does request have basic authentication info in the "Authorization" header
+        guard let basic = request.headers.basic else { return nil }
+
+        // check if user exists and then verify the entered password against the one stored in the database.
+        // If it is correct then login in user
+        let user = try await users.getUser(named: basic.username)
+        guard let user, let passwordHash = user.passwordHash else { return nil }
+        // Do Bcrypt verify on a separate thread to not block the general task executor
+        guard try await NIOThreadPool.singleton.runIfActive({ Bcrypt.verify(basic.password, hash: passwordHash) }) else { return nil }
+        return user
+    }
+}
