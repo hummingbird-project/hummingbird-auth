6a7d8559050d7121be8eb1eaf4ae12f54ced7d04
Adam Fowler <adamfowler71@gmail.com>
2024-09-23 10:06:36 +0100

    Comments

--- Sources/HummingbirdAuth/Sessions/SessionContext.swift
+++ Sources/HummingbirdAuth/Sessions/SessionContext.swift
@@ -16,20 +16,24 @@ import Foundation
 import Hummingbird
 import NIOConcurrencyHelpers
 
+/// Session data
 @dynamicMemberLookup
 public struct SessionData<Session: Sendable & Codable>: Codable, Sendable {
     @usableFromInline
     var object: Session
     @usableFromInline
     var edited: Bool
+    /// When session will expire
     public var expiresIn: Duration?
 
+    @usableFromInline
     init(value: Session, expiresIn: Duration?) {
         self.object = value
         self.edited = true
         self.expiresIn = expiresIn
     }
 
+    /// Codable decode initializer
     public init(from decoder: any Decoder) throws {
         let container = try decoder.singleValueContainer()
         self.object = try container.decode(Session.self)
@@ -37,11 +41,14 @@ public struct SessionData<Session: Sendable & Codable>: Codable, Sendable {
         self.expiresIn = nil
     }
 
+    /// Codable encode
     public func encode(to encoder: any Encoder) throws {
         var container = encoder.singleValueContainer()
         try container.encode(self.object)
     }
 
+    /// Dynamic member lookup. Sets `edited` state if value in `object` is mutated.
+    @inlinable
     public subscript<T>(dynamicMember keyPath: WritableKeyPath<Session, T>) -> T {
         get {
             self.object[keyPath: keyPath]
@@ -53,52 +60,68 @@ public struct SessionData<Session: Sendable & Codable>: Codable, Sendable {
     }
 }
 
+/// Session context
+///
+/// Holds reference to session data protected by a lock to avoid concurrent access
 public struct SessionContext<Session: Sendable & Codable>: Sendable {
     @usableFromInline
     let _storage: NIOLockedValueBox<SessionData<Session>?>
 
+    /// Initialize `SessionContext`
     @inlinable
-    public init() {
+    init() {
         self._storage = .init(nil)
     }
 
+    ///  Set session data
+    /// - Parameters:
+    ///   - session: Session data
+    ///   - expiresIn: How long before session data expires
+    @inlinable
     public func setSession(_ session: Session, expiresIn: Duration? = nil) {
         self._storage.withLockedValue {
             $0 = .init(value: session, expiresIn: expiresIn)
         }
     }
 
+    ///  Clear session data
+    @inlinable
     public func clearSession() {
         self._storage.withLockedValue {
             $0 = nil
         }
     }
 
+    /// Get a copy of the session data
+    @inlinable
     public var session: Session? { self._storage.withLockedValue { $0?.object } }
 
+    /// Access the session and allowing it to be mutated
+    @inlinable
     public func withLockedSession<Value>(_ mutate: (inout SessionData<Session>?) -> Value) -> Value {
         self._storage.withLockedValue {
             mutate(&$0)
         }
     }
 
-    func setSessionData(_ sessionData: SessionData<Session>) {
-        self._storage.withLockedValue {
-            $0 = sessionData
-        }
-    }
-
-    func getSessionData() -> SessionData<Session>? {
-        self._storage.withLockedValue { $0 }
+    /// Internal access to full session data. Used by `SessionMiddleware`.
+    var sessionData: SessionData<Session>? {
+        get { self._storage.withLockedValue { $0 }}
+        nonmutating set { self._storage.withLockedValue { $0 = newValue }}
     }
 }
 
+/// Protocol for RequestContext that stores session data
+///
+/// The `Session` associatedtype is the data stored in your session. This could be
+/// as simple as a `UUID`` that is used to extract a user from a database to a
+/// struct containing support for multiple authentication flows.
 public protocol SessionRequestContext<Session>: RequestContext {
     associatedtype Session: Sendable & Codable
     var sessions: SessionContext<Session> { get }
 }
 
-/// Implementation of a basic request context that supports everything the Hummingbird library needs
+/// Implementation of a basic request context that supports session storage and authenticators
 public struct BasicSessionRequestContext<Session>: AuthRequestContext, SessionRequestContext, RequestContext where Session: Sendable & Codable {
     /// core context
     public var coreContext: CoreRequestContextStorage
--- Sources/HummingbirdAuth/Sessions/SessionMiddleware.swift
+++ Sources/HummingbirdAuth/Sessions/SessionMiddleware.swift
@@ -14,40 +14,62 @@
 
 import Hummingbird
 
+/// Middleware that extracts session data for a request and stores it in the context
+///
+/// The `SessionMiddleware` requires that the request context conform to ``SessionRequestContext``.
+///
+/// The middleware extracts the session data from storage based off the session cookie in the
+/// request. If after returning from running the rest of the middleware chain the session data
+/// is flagged as being edited it will save it into the session storage and in the case this is
+/// a new session will set the "Set-Cookie" header.
 public struct SessionMiddleware<Context: SessionRequestContext>: RouterMiddleware {
+    /// storage for session data
     let sessionStorage: SessionStorage
+    /// default duration for a session token
     let defaultSessionExpiration: Duration
 
+    /// Initialize SessionMiddleware
+    /// - Parameters:
+    ///   - storage: Persist driver to use for storage
+    ///   - sessionCookie: Session cookie name
+    ///   - defaultSessionExpiration: Default expiration for session data
     public init(storage: any PersistDriver, sessionCookie: String = "SESSION_ID", defaultSessionExpiration: Duration = .seconds(60 * 60 * 12)) {
         self.sessionStorage = .init(storage, sessionCookie: sessionCookie)
         self.defaultSessionExpiration = defaultSessionExpiration
     }
 
+    ///  Initialize Session Middleware
+    /// - Parameters:
+    ///   - sessionStorage: Session storage
+    ///   - defaultSessionExpiration: Default expiration for session data
     public init(sessionStorage: SessionStorage, defaultSessionExpiration: Duration = .seconds(60 * 60 * 12)) {
         self.sessionStorage = sessionStorage
         self.defaultSessionExpiration = defaultSessionExpiration
     }
 
+    ///  Session Middleware handler
     public func handle(_ request: Request, context: Context, next: (Request, Context) async throws -> Response) async throws -> Response {
-        let sessionData = try await sessionStorage.load(as: SessionData<Context.Session>.self, request: request)
-        if let sessionData {
-            context.sessions.setSessionData(sessionData)
+        let originalSessionData = try await sessionStorage.load(as: SessionData<Context.Session>.self, request: request)
+        if let originalSessionData {
+            context.sessions.sessionData = originalSessionData
         }
         var response = try await next(request, context)
-        let session = context.sessions.getSessionData()
-        if let session {
-            if session.edited {
-                let expiresIn = session.expiresIn ?? self.defaultSessionExpiration
+        let sessionData = context.sessions.sessionData
+        if let sessionData {
+            // if session has been edited then store new session
+            if sessionData.edited {
+                let expiresIn = sessionData.expiresIn ?? self.defaultSessionExpiration
                 do {
-                    try await self.sessionStorage.update(session: session, expiresIn: expiresIn, request: request)
+                    try await self.sessionStorage.update(session: sessionData, expiresIn: expiresIn, request: request)
                 } catch let error as SessionStorage.Error where error == .sessionDoesNotExist {
-                    let cookie = try await self.sessionStorage.save(session: session, expiresIn: expiresIn)
+                    let cookie = try await self.sessionStorage.save(session: sessionData, expiresIn: expiresIn)
+                    // this is a new session so set the "Set-Cookie" header
                     response.headers[values: .setCookie].append(cookie.description)
                 }
             }
-        } else if sessionData != nil {
+        } else if originalSessionData != nil {
             // if we had a session and we don't anymore, set session to expire
-            try await self.sessionStorage.update(session: session, expiresIn: .seconds(0), request: request)
+            try await self.sessionStorage.update(session: sessionData, expiresIn: .seconds(0), request: request)
         }
         return response
     }
